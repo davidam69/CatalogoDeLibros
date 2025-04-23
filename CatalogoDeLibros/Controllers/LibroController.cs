f2b6052b650fbea4e25c6f3ebfe59ca19b1113d1
david Miranda <david_m_pericon@hotmail.com>
2025-04-23 16:28:27 -0300

    combinacion de cambios en LibroController

--- CatalogoDeLibros/Controllers/LibroController.cs
+++ CatalogoDeLibros/Controllers/LibroController.cs
@@ -5,17 +5,20 @@ namespace CatalogoDeLibros.Controllers
 {
     public class LibroController : Controller
     {
-        public IActionResult Index()
+        private List<Libro> ObtenerLibros()
         {
             var autor1 = new Autor { id = 1, nombre = "George Orwell" };
             var autor2 = new Autor { id = 2, nombre = "Ray Bradbury" };
-
-            var libros = new List<Libro>
+            return new List<Libro>
             {
                 new Libro { id = 1, titulo = "1984", anioPublicacion = 1949, autor = autor1 },
                 new Libro { id = 2, titulo = "Fahrenheit 451", anioPublicacion = 1953, autor = autor2 },
                 new Libro { id = 3, titulo = "Rebelión en la granja", anioPublicacion = 1945, autor = autor1 }
             };
+        }
+        public IActionResult Index()
+        {
+            var libros = ObtenerLibros();
             return View(libros);
         }
     }
