9c731ca54db0dbc32334d38bb48ff56b7b7b0379
david Miranda <david_m_pericon@hotmail.com>
2025-04-24 00:39:24 -0300

    se agrego portadas de los libros

--- CatalogoDeLibros/Controllers/LibroController.cs
+++ CatalogoDeLibros/Controllers/LibroController.cs
@@ -11,9 +11,9 @@ namespace CatalogoDeLibros.Controllers
             var autor2 = new Autor { id = 2, nombre = "Ray Bradbury" };
             return new List<Libro>
             {
-                new Libro { id = 1, titulo = "1984", anioPublicacion = 1949, autor = autor1 },
-                new Libro { id = 2, titulo = "Fahrenheit 451", anioPublicacion = 1953, autor = autor2 },
-                new Libro { id = 3, titulo = "Rebelión en la granja", anioPublicacion = 1945, autor = autor1 }
+                new Libro { id = 1, titulo = "1984", anioPublicacion = 1949, autor = autor1, UrlImagen = "https://proassetspdlcom.cdnstatics2.com/usuaris/libros/thumbs/b59f709a-ce74-4e50-a3a3-3527d68da121/d_360_620/portada_1984_george-orwell_202102151044.webp" },
+                new Libro { id = 2, titulo = "Fahrenheit 451", anioPublicacion = 1953, autor = autor2, UrlImagen ="https://upload.wikimedia.org/wikipedia/en/d/db/Fahrenheit_451_1st_ed_cover.jpg" },
+                new Libro { id = 3, titulo = "Rebelión en la granja", anioPublicacion = 1945, autor = autor1, UrlImagen = "https://proassetspdlcom.cdnstatics2.com/usuaris/libros/thumbs/a1ebb647-56f6-4c85-9c2e-a6f9ebb1ef27/d_360_620/portada_rebelion-en-la-granja_george-orwell_202104141022.webp" }
             };
         }
         public IActionResult Index()
--- CatalogoDeLibros/Models/Libro.cs
+++ CatalogoDeLibros/Models/Libro.cs
@@ -6,10 +6,11 @@
         public string? titulo { get; set; }
         public int anioPublicacion { get; set; }
         public Autor? autor { get; set; }
+        public string? UrlImagen { get; set; }
+
         // public string? editorial { get; set; }
         // public string? genero { get; set; }
         // public string? sinopsis { get; set; }
-        // public string? portada { get; set; }
         // public bool disponible { get; set; } = true;
     }
 }
