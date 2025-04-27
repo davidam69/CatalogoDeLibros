c6cbd8017f7d1c01bcc190f1527ed8ecfd889f66
david <david_m_pericon@hotmail.com>
2025-04-27 16:32:20 -0300

    muchos cambios color de fondo, link de detalle en autor etc

--- CatalogoDeLibros/Controllers/LibroController.cs
+++ CatalogoDeLibros/Controllers/LibroController.cs
@@ -5,17 +5,37 @@ namespace CatalogoDeLibros.Controllers
 {
     public class LibroController : Controller
     {
-        private List<Libro> ObtenerLibros()
+        private static List<Libro> ObtenerLibros()
         {
-            var autor1 = new Autor { id = 1, nombre = "George Orwell" };
-            var autor2 = new Autor { id = 2, nombre = "Ray Bradbury" };
             return new List<Libro>
             {
-                new Libro { id = 1, titulo = "1984", anioPublicacion = 1949, autor = autor1, UrlImagen = "https://proassetspdlcom.cdnstatics2.com/usuaris/libros/thumbs/b59f709a-ce74-4e50-a3a3-3527d68da121/d_360_620/portada_1984_george-orwell_202102151044.webp" },
-                new Libro { id = 2, titulo = "Fahrenheit 451", anioPublicacion = 1953, autor = autor2, UrlImagen ="https://upload.wikimedia.org/wikipedia/en/d/db/Fahrenheit_451_1st_ed_cover.jpg" },
-                new Libro { id = 3, titulo = "Rebelión en la granja", anioPublicacion = 1945, autor = autor1, UrlImagen = "https://proassetspdlcom.cdnstatics2.com/usuaris/libros/thumbs/a1ebb647-56f6-4c85-9c2e-a6f9ebb1ef27/d_360_620/portada_rebelion-en-la-granja_george-orwell_202104141022.webp" }
+                new Libro
+                {
+                    id = 1,
+                    titulo = "1984",
+                    anioPublicacion = 1949,
+                    autor = new Autor { id = 1, nombre = "George Orwell" },
+                    UrlImagen = "https://proassetspdlcom.cdnstatics2.com/usuaris/libros/thumbs/b59f709a-ce74-4e50-a3a3-3527d68da121/d_360_620/portada_1984_george-orwell_202102151044.webp"
+                },
+                new Libro
+                {
+                    id = 2,
+                    titulo = "Fahrenheit 451",
+                    anioPublicacion = 1953,
+                    autor = new Autor { id = 2, nombre = "Ray Bradbury" },
+                    UrlImagen = "https://upload.wikimedia.org/wikipedia/en/d/db/Fahrenheit_451_1st_ed_cover.jpg"
+                },
+                new Libro
+                {
+                    id = 3,
+                    titulo = "Rebelión en la granja",
+                    anioPublicacion = 1945,
+                    autor = new Autor { id = 1, nombre = "George Orwell" },
+                    UrlImagen = "https://proassetspdlcom.cdnstatics2.com/usuaris/libros/thumbs/a1ebb647-56f6-4c85-9c2e-a6f9ebb1ef27/d_360_620/portada_rebelion-en-la-granja_george-orwell_202104141022.webp"
+                }
             };
         }
+
         public IActionResult Index()
         {
             var libros = ObtenerLibros();
@@ -47,6 +67,14 @@ namespace CatalogoDeLibros.Controllers
 
             return View("Autor", librosAutor);
         }
+
+        public IActionResult CambiarFondo(string color)
+        {
+            ViewBag.ColorFondo = color;
+            var libros = ObtenerLibros();
+            return View("Index", libros);
+        }
+
 
     }
 }
