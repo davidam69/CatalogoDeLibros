acc683c735951e2dd2351ba3c1d2ff5f3170904d
david Miranda <david_m_pericon@hotmail.com>
2025-05-07 01:10:37 -0300

    borrado de datos que ya no sirven

--- CatalogoDeLibros/Controllers/LibroController.cs
+++ CatalogoDeLibros/Controllers/LibroController.cs
@@ -2,39 +2,6 @@
 {
     public class LibroController : Controller
     {
-        /*private static List<Libro> ObtenerLibros()
-        {
-            return new List<Libro>
-            {
-                new Libro
-                {
-                    id = 1,
-                    titulo = "1984",
-                    anioPublicacion = 1949,
-                    autor = new Autor { id = 1, nombre = "George Orwell" },
-                    autorId = 1,
-                    UrlImagen = "https://proassetspdlcom.cdnstatics2.com/usuaris/libros/thumbs/b59f709a-ce74-4e50-a3a3-3527d68da121/d_360_620/portada_1984_george-orwell_202102151044.webp"
-                },
-                new Libro
-                {
-                    id = 2,
-                    titulo = "Fahrenheit 451",
-                    anioPublicacion = 1953,
-                    autor = new Autor { id = 2, nombre = "Ray Bradbury" },
-                    autorId = 2,
-                    UrlImagen = "https://upload.wikimedia.org/wikipedia/en/d/db/Fahrenheit_451_1st_ed_cover.jpg"
-                },
-                new Libro
-                {
-                    id = 3,
-                    titulo = "Rebelión en la granja",
-                    anioPublicacion = 1945,
-                    autor = new Autor { id = 1, nombre = "George Orwell" },
-                    autorId = 1,
-                    UrlImagen = "https://proassetspdlcom.cdnstatics2.com/usuaris/libros/thumbs/a1ebb647-56f6-4c85-9c2e-a6f9ebb1ef27/d_360_620/portada_rebelion-en-la-granja_george-orwell_202104141022.webp"
-                }
-            };
-        }*/
         private static List<Libro> _libros = new List<Libro>
         {
             new Libro
@@ -82,20 +49,6 @@
             var libros = ObtenerLibros();
             return View(libros);
         }
-
-       /* public IActionResult Detalle(int id)
-        {
-            ViewBag.ColorFondo = TempData["ColorFondo"] ?? "white";
-            TempData.Keep("ColorFondo"); // mantiene TempData para futuras páginas
-            var libros = ObtenerLibros();
-            var libro = libros.FirstOrDefault(l => l.id == id);
-            if (libro == null)
-            {
-                ViewBag.Error = "El libro no fue encontrado.";
-                return View("Error");
-            }
-            return View(libro);
-        }*/
 
         public IActionResult Detalle(int id)
         {
